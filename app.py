a038e9ff59ce9196377616f52da72b8a0ed93c92
Azim-js <az1234im@gmail.com>
2021-03-05 17:42:03 +0530

    desging using the kivy file .kv

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import kivy
-kivy.require('1.9.0')
+# kivy.require('1.9.0 ')
+from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.button import Label
 
+Builder.load_file("design.kv")
+
 class HelloApp(App):
     def build(Self):
-        return Label(text="Hello World!")
+        return Label()
 
 if __name__=="__main__":
     HelloApp().run()

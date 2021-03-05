afd2973ef7870f99d392aba3ea51cb21d784c2b3
Azim-js <az1234im@gmail.com>
2021-03-05 11:26:55 +0530

    commit 2

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello")
+import kivy
+kivy.require('1.9.0')
+from kivy.app import App
+from kivy.uix.button import Label
+
+class HelloApp(App):
+    def build(Self):
+        return Label(text="Hello World!")
+
+if __name__=="__main__":
+    HelloApp().run()
